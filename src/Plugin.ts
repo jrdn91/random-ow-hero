0c1ee485410e644ab59129f1ec8e9cd8f37ac05e
Jordan Riser <riser.jordan@gmail.com>
2023-01-14 15:26:45 -0500

    loads overwatch heroes on receiving settings

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -2,9 +2,24 @@ import { Streamdeck } from '@rweich/streamdeck-ts';
 
 const plugin = new Streamdeck().plugin();
 
+type Hero = {
+  key: string;
+  name: string;
+  portrait: string;
+  role: 'tank' | 'dps' | 'support';
+};
+
+let heroArray: Hero[] = [];
 // your code here..
 plugin.on('willAppear', ({ context }) => {
   plugin.setTitle('test', context);
 });
 
+plugin.on('didReceiveSettings', async () => {
+  const heroResponse = await fetch('https://overfast-api.tekrop.fr/heroes');
+  if (heroResponse.ok) {
+    const heroes = await heroResponse.json();
+    heroArray = heroes;
+  }
+});
 export default plugin;
